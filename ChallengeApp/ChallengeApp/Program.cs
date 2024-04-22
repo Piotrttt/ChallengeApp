eecfbf528759d99092588c36eb86ef1db07ce2b0
Piotrttt <ptyczynski@gmail.com>
2024-04-23 01:50:10 +0200

    Praca domowa dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// praca domowa dzień 4
+using System.Security.Cryptography;
+
+var name = "Joanna";
+var age = 42;
+var sex = "female";
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, 30 lat");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
